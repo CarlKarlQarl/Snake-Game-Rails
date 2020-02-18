85645969f628ec6896310984103134576ac87242
CarlKarlQarl <karlmatthes@gmail.com>
2020-02-18 10:52:42 -0700

    testing cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://snake-game-react-77b60.firebaseapp.com/'
+    origins 'http://localhost:3000'
 
     resource '*',
       headers: :any,
