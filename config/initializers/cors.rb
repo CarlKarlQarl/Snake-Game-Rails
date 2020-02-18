cd335ced253b5cc7ec78aaac056ce65f01692539
CarlKarlQarl <karlmatthes@gmail.com>
2020-02-18 11:29:16 -0700

    switch cors origin to firebase

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['https://snake-game-react-77b60.firebaseapp.com/']
+    origins 'https://snake-game-react-77b60.firebaseapp.com'
 
     resource '*',
       headers: :any,
